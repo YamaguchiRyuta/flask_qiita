53d3a57a46fd1d7c4f78388b4644b13f74d8f4f2
山口隆太 <steven_siro_vai@hotmail.co.jp>
2022-02-04 15:30:52 +0900

    edit

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ api = Api(app, version='1.0', title='Sample API', description='A sample API')
 
 # Flask-SQLAlchemyの設定
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/my_schema?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@127.0.0.1/my_schema?charset=utf8mb4'
 db = SQLAlchemy(app)
 
 # Flask-Migrateの設定
